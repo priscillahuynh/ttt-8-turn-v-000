b3ddc5eb3aaa0be698e4972f26ce1104b2204476
Priscilla H <priscillamhuynh@gmail.com>
2018-06-07 06:33:31 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,11 +1,25 @@
-board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+board = ["X", "X", "X", "X", "O", "O", "X", "O", "O"]
 
 def display_board(board)
-board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
-  puts " X | X | X "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts " X | O | O "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts " X | O | O "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 display_board(board)
+
+
+
+
+# board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+# 
+# def display_board(board)
+# board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
+#   puts " X | X | X "
+#   puts "-----------"
+#   puts " X | O | O "
+#   puts "-----------"
+#   puts " X | O | O "
+# end
+# display_board(board)
