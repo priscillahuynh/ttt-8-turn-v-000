a9fdb6fce0032ea9b859ebcbd587ba09cb5e22b1
Priscilla H <priscillamhuynh@gmail.com>
2018-06-07 06:34:10 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -8,16 +8,3 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 display_board(board)
-
-
-# board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
-#
-# def display_board(board)
-# board = ["X", "O", "X", "O", "X", "X", "O", "X", "O"]
-#   puts " X | X | X "
-#   puts "-----------"
-#   puts " X | O | O "
-#   puts "-----------"
-#   puts " X | O | O "
-# end
-# display_board(board)
