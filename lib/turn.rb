aeebcc7f37887b747e8d9aadc49c1f9410792b56
Priscilla H <priscillamhuynh@gmail.com>
2018-06-07 06:23:03 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,6 +1,7 @@
 board=["   ","   ","   ","   ","   ","   ","   ","   ","   "]
 
 def display_board(board)
+board=["   ","   ","   ","   ","   ","   ","   ","   ","   "]
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
